03cada3272b955cf6b86d15c99718d3e5fe86599
Björn Benouarets <bbenouarets@proton.me>
2025-05-16 11:18:19 +0200

    Update notion init

--- src/notion/__init__.py
+++ src/notion/__init__.py
@@ -1,3 +1,5 @@
-from .api import NotionApiClient
+from .api.client.client import Client
+from . import components as Components
+from . import properties as Properties
 
-__all__ = ["NotionApiClient"]
+__all__ = ["Client", "Components", "Properties"]
